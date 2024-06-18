710fd229652bc32ac1c983ad0b585448d3b7d527
AMRWERY <amrmounir2@gmail.com>
2024-06-18 18:16:38 +0300

    handle validations

--- dynamic-forms/dynamic-inputs.js
+++ dynamic-forms/dynamic-inputs.js
@@ -3,7 +3,7 @@ const loginSchema = [
     $formkit: "email",
     name: "email",
     label: "Email",
-    validation: "required|email",
+    validation: "required|email|ends_with:.com",
     outerClass:
       "px-3 py-2 text-gray-800 transition duration-100 rounded ring-indigo-300 focus:ring",
     innerClass: "outline-none bg-gray-50 border w-full",
@@ -24,7 +24,7 @@ const registrationSchema = [
     $formkit: "text",
     name: "firstName",
     label: "First Name",
-    validation: "required",
+    validation: "required|contains_numeric|length:3,10",
     outerClass:
       "px-3 py-2 text-gray-800 transition duration-100 rounded ring-indigo-300 focus:ring",
     innerClass: "outline-none bg-gray-50 border w-full",
@@ -33,7 +33,7 @@ const registrationSchema = [
     $formkit: "text",
     name: "lastName",
     label: "Last Name",
-    validation: "required",
+    validation: "required|contains_numeric|length:3,10",
     outerClass:
       "px-3 py-2 text-gray-800 transition duration-100 rounded ring-indigo-300 focus:ring",
     innerClass: "outline-none bg-gray-50 border w-full",
@@ -42,7 +42,7 @@ const registrationSchema = [
     $formkit: "email",
     name: "email",
     label: "Email",
-    validation: "required|email",
+    validation: "required|email|ends_with:.com",
     outerClass:
       "px-3 py-2 text-gray-800 transition duration-100 rounded ring-indigo-300 focus:ring",
     innerClass: "outline-none bg-gray-50 border w-full",
