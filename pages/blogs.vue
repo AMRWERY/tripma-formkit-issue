856db7072d1a0457a3d15b415a49dcf714e55c7b
AMRWERY <amrmounir2@gmail.com>
2024-06-21 17:54:52 +0300

    handle blogs translations

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -5,11 +5,12 @@
         <div class="max-w-screen-xl px-4 mx-auto md:px-8">
           <!-- text - start -->
           <div class="mb-10 md:mb-16">
-            <h2 class="mb-4 text-2xl font-bold text-center text-gray-800 md:mb-6 lg:text-3xl">Blogs</h2>
-
-            <p class="max-w-screen-md mx-auto text-center text-gray-500 md:text-lg">This is a section of some simple
-              filler text, also known as placeholder text. It shares some characteristics of a real written text but is
-              random or otherwise generated.</p>
+            <h2 class="mb-4 text-2xl font-bold text-center text-gray-800 md:mb-6 lg:text-3xl">{{ $t('layout.blogs') }}
+            </h2>
+            <p class="max-w-screen-md mx-auto text-center text-gray-500 md:text-lg">{{
+              $t('blogs.this_is_a_section_of_some_simple_filler_text_also_known_as_placeholder_text_it') }} {{
+                $t('blogs.shares_some_characteristics_of_a_real_written_text_but_is_random_or_otherwise') }} {{
+                $t('blogs.generated') }}</p>
           </div>
           <!-- text - end -->
 
@@ -23,21 +24,21 @@
               </nuxt-link>
 
               <div class="flex flex-col gap-2 p-4 lg:p-6">
-                <span class="text-sm text-gray-400">July 19, 2024</span>
+                <span class="text-sm text-gray-400">{{ $t('blogs.july_19_2024') }}</span>
 
                 <h2 class="text-xl font-bold text-gray-800">
-                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">New
-                    trends in
-                    Tech</nuxt-link>
+                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{
+                    $t('blogs.new_trends_in_tech') }}</nuxt-link>
                 </h2>
 
-                <p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.
+                <p class="text-gray-500">{{
+                  $t('blogs.this_is_a_section_of_some_simple_filler_text_also_known_as_placeholder_text') }}
                 </p>
 
                 <div>
                   <nuxt-link to="#"
-                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Read
-                    more</nuxt-link>
+                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">{{
+                      $t('blogs.read_more') }}</nuxt-link>
                 </div>
               </div>
             </div>
@@ -52,21 +53,21 @@
               </nuxt-link>
 
               <div class="flex flex-col gap-2 p-4 lg:p-6">
-                <span class="text-sm text-gray-400">April 07, 2024</span>
+                <span class="text-sm text-gray-400">{{ $t('blogs.april_07_2024') }}</span>
 
                 <h2 class="text-xl font-bold text-gray-800">
-                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">Working
-                    with
-                    legacy stacks</nuxt-link>
+                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{
+                    $t('blogs.Working_with_legacy_stacks') }}</nuxt-link>
                 </h2>
 
-                <p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.
+                <p class="text-gray-500">{{
+                  $t('blogs.this_is_a_section_of_some_simple_filler_text_also_known_as_placeholder_text') }}
                 </p>
 
                 <div>
                   <nuxt-link to="#"
-                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Read
-                    more</nuxt-link>
+                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">{{
+                      $t('blogs.read_more') }}</nuxt-link>
                 </div>
               </div>
             </div>
@@ -81,20 +82,21 @@
               </nuxt-link>
 
               <div class="flex flex-col gap-2 p-4 lg:p-6">
-                <span class="text-sm text-gray-400">March 15, 2024</span>
+                <span class="text-sm text-gray-400">{{ $t('blogs.march_15_2024') }}</span>
 
                 <h2 class="text-xl font-bold text-gray-800">
-                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">10 best
-                    smartphones for devs</nuxt-link>
+                  <nuxt-link to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{
+                    $t('blogs.10_best_smartphones_for_devs') }}</nuxt-link>
                 </h2>
 
-                <p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.
+                <p class="text-gray-500">{{
+                  $t('blogs.this_is_a_section_of_some_simple_filler_text_also_known_as_placeholder_text') }}
                 </p>
 
                 <div>
                   <nuxt-link to="#"
-                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Read
-                    more</nuxt-link>
+                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">{{
+                      $t('blogs.read_more') }}</nuxt-link>
                 </div>
               </div>
             </div>
@@ -109,20 +111,21 @@
               </nuxt-link>
 
               <div class="flex flex-col gap-2 p-4 lg:p-6">
-                <span class="text-sm text-gray-400">January 27, 2024</span>
+                <span class="text-sm text-gray-400">{{ $t('blogs.january_27_2024') }}</span>
 
                 <h2 class="text-xl font-bold text-gray-800">
-                  <a to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">8 High
-                    performance Notebooks</a>
+                  <a to="#" class="transition duration-100 hover:text-indigo-500 active:text-indigo-600">{{
+                    $t('blogs.8_high_performance_notebooks') }}</a>
                 </h2>
 
-                <p class="text-gray-500">This is a section of some simple filler text, also known as placeholder text.
+                <p class="text-gray-500">{{
+                  $t('blogs.this_is_a_section_of_some_simple_filler_text_also_known_as_placeholder_text') }}
                 </p>
 
                 <div>
                   <nuxt-link to="#"
-                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Read
-                    more</nuxt-link>
+                    class="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">{{
+                      $t('blogs.read_more') }}</nuxt-link>
                 </div>
               </div>
             </div>
