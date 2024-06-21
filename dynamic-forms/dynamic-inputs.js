4d0ed256748e8db4e56203d22c21c474b045e630
AMRWERY <amrmounir2@gmail.com>
2024-06-21 18:09:32 +0300

    add translations to contact_us form

--- dynamic-forms/dynamic-inputs.js
+++ dynamic-forms/dynamic-inputs.js
@@ -84,7 +84,7 @@ const contactUsSchema = [
   {
     $formkit: "textarea",
     name: "textarea",
-    label: "Message",
+    label: "forms.message",
     validation: "required|length:0,120",
     outerClass:
       "px-3 py-2 text-gray-800 transition duration-100 rounded ring-indigo-300 focus:ring",
