9062b6973e8b596540a36d3fa1fb46238390921a
AMRWERY <amrmounir2@gmail.com>
2024-06-21 18:09:44 +0300

    ..

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -10,21 +10,23 @@
         <div class="flex px-5 py-24 mx-auto">
           <div
             class="relative z-10 flex flex-col w-full p-8 mt-10 bg-white rounded-lg shadow-md lg:w-1/3 md:w-1/2 md:ml-auto md:mt-0">
-            <h2 class="mb-1 text-lg font-semibold text-center text-gray-900 title-font">Get in touch</h2>
-            <p class="mb-5 font-medium text-center text-gray-900 text-md">Got a technical issue? Want to send feedback
-              about
-              a beta feature? Need details about our Business plan? Let us know.</p>
+            <h2 class="mb-1 text-lg font-semibold text-center text-gray-900 title-font">{{ $t('contact_us.get_in_touch')
+              }}</h2>
+            <p class="mb-5 font-medium text-center text-gray-900 text-md">{{
+              $t('contact_us.got_a_technical_issue_want_to_send_feedback') }} {{
+                $t('contact_us.about_a_beta_feature_need_details_about_our') }} {{
+                $t('contact_us.business_plan_let_us_know') }}</p>
 
             <FormKit type="form" id="reset-password-form" v-model="data" :actions="false" :incomplete-message="false">
               <div class="relative mb-4">
-                <FormKitSchema :schema="contactUsSchema" />
+                <FormKitSchema :schema="translatedContactUsSchema" />
               </div>
               <button type="submit"
-                class="block w-full px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">Let's
-                talk</button>
+                class="block w-full px-6 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">{{
+                  $t('buttons.lets_talk') }}</button>
             </FormKit>
 
-            <p class="mt-3 text-xs text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p class="mt-3 text-xs text-gray-500">{{ $t('contact_us.from_small_beginnings_come_great_things') }}</p>
           </div>
         </div>
       </div>
@@ -39,6 +41,10 @@ const data = reactive({
   email: '',
   message: ''
 });
+
+const { t } = useI18n();
+
+const translatedContactUsSchema = computed(() => translateSchemaLabels(contactUsSchema, t));
 </script>
 
 <style scoped>
