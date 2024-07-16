530f01a194d2d8cdc1628cbaae02dbe0e64ea15c
AMRWERY <amrmounir2@gmail.com>
2024-07-16 17:23:52 +0300

    handle overview-tab

--- components/tabs/overview-tab.vue
+++ components/tabs/overview-tab.vue
@@ -28,19 +28,54 @@
         <div class="mt-10">
           <p class="text-xl font-semibold">Top amenities</p>
           <div class="flex text-sm">
-            <div class="flex-1 p-4 m-2">
+            <div class="flex-1 p-4">
               <icon name="material-symbols:wifi-sharp" size="20px" />
               <span class="font-normal ms-2">Free WiFi</span>
             </div>
-            <div class="flex-1 p-4 m-2">
+            <div class="flex-1 p-4">
               <icon name="ph:swimming-pool-fill" size="20px" />
               <span class="font-normal ms-2">Pool</span>
             </div>
-            <div class="flex-1 p-4 m-2">
+            <div class="flex-1 p-4">
               <icon name="icon-park-solid:spa-candle" size="20px" />
               <span class="font-normal ms-2">Spa</span>
             </div>
           </div>
+          <div class="flex text-sm">
+            <div class="flex-1 p-4">
+              <icon name="fluent:vehicle-car-parking-48-regular" size="20px" />
+              <span class="font-normal ms-2">Parking</span>
+            </div>
+            <div class="flex-1 p-4">
+              <icon name="wi:day-cloudy-gusts" size="20px" />
+              <span class="font-normal ms-2">A/C</span>
+            </div>
+            <div class="flex-1 p-4">
+              <icon name="ic:sharp-restaurant-menu" size="20px" />
+              <span class="font-normal ms-2">Restaurant</span>
+            </div>
+          </div>
+          <div class="flex text-sm">
+            <div class="flex-1 p-4">
+              <icon name="material-symbols-light:local-bar-rounded" size="20px" />
+              <span class="font-normal ms-2">Hotel bar</span>
+            </div>
+            <div class="flex-1 p-4">
+              <icon name="iconoir:gym" size="20px" />
+              <span class="font-normal ms-2">GYM</span>
+            </div>
+            <div class="flex-1 p-4">
+              <icon name="material-symbols:pets" size="20px" />
+              <span class="font-normal ms-2">Pets</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="relative h-[280px] w-[380px] ms-auto">
+          <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3454.8539354086974!2d31.307682574902486!3d29.98602286552143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458391b0156a9b1%3A0x35b0bda415fdbe15!2sCarrefour%20-%20Maadi%20City%20Center!5e0!3m2!1sen!2seg!4v1673950981424!5m2!1sen!2seg"
+            class="custom-map-style"></iframe>
         </div>
       </div>
     </div>
